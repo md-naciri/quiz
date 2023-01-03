be132a1eef888ff7a864f2ddcc3fd4af2314b5f0
md_naciri <med.ar.naciri@gmail.com>
2023-01-03 12:09:47 +0100

    JPQUIZ-10 modify the script to work with a json file-FrontEnd Part-

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,45 +6,27 @@ let count = 0;
 let corrections = [];
 let correction = "";
 let answered = "";
-const countDown = document.getElementById("countdown");
-let time = 30;
+// const countDown = document.getElementById("countdown");
+// let time = 30;
 circle[index].style.background = "#232f3e";
 circle[index].style.color = 'white';
 
-function beforeResult() {
+// let lengthtable=0;
+
+
+function beforeResult(mydata) {
     nextStep();
     $("#questions").hide();
     $(".progress-bar-container").hide();
     $("#result").show();
-    document.getElementById("score").innerHTML = count * 100 / questions.length;
+    document.getElementById("score").innerHTML = count * 100 / mydata.length;
     corrections.forEach(element => {
-        correction += `<div class="container col-lg-6 col-md-8 col-sm-10 shadow p-3 my-2 bg-white rounded"><p class="quest-text"><strong>Question:</strong> ${element.question}</p><p class="right-answer"><strong>Right answer:</strong> ${element.answer}</p></div>`;
+        correction += `<div class="container col-lg-6 col-md-8 col-sm-10 shadow p-3 my-2 bg-white rounded"><p class="quest-text"><strong>Question:</strong> ${element[0]}</p><p class="right-answer"><strong>Right answer:</strong> ${element[1]}</p></div>`;
     });
     document.getElementById("correction").innerHTML = correction;
 }
 
-function downTimer() {
-    countDown.innerHTML = time;
-    if (time < 10){
-        countDown.textContent = "0" + time;
-    }
-    time--;
-    if (time >= 0) {
-        setTimeout(downTimer, 1000);
-    } else {
-        corrections.push({ question: questions[k].question, answer: questions[k].answer.correction });
-        k++;
-        if (k < questions.length) {
-            time = 30;
-            GetQeustion();
-            downTimer();
-        }
-        else {
-            beforeResult();
-        }
 
-    }
-}
 
 function nextStep() {
     if (index < 3) {
@@ -62,8 +44,9 @@ $(document).ready(function () {
     $("#result").hide();
     $("#show-correction").hide();
     $("#start").click(function () {
+        GetQeustion();
+        // downTimer();
         nextStep();
-        downTimer();
         $("#questions").show();
         $("#info").hide();
         $(".progress-bar-container").show();
@@ -73,67 +56,89 @@ $(document).ready(function () {
 let choice = document.querySelectorAll('.choice');
 let j = 0;
 let k = 0;
-questions.sort(function () { return Math.random() - 0.5 });
 function GetQeustion() {
-    document.getElementById("quest").innerHTML = questions[k].question;
-    choice.forEach(option => {
-        option.innerHTML = questions[k].options[j].letter;
-        option.innerHTML += ") " + questions[k].options[j].text;
-        j++;
-    });
-    j = 0;
-    progressBar.style.width = `${100 * (k + 1) / questions.length}%`;
+    var mydata = new XMLHttpRequest();
+
+    mydata.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            mydata = JSON.parse(this.responseText);
+            // mydata.sort(function () { return Math.random() - 0.5 });
+            // lengthtable = mydata.length;
+            document.getElementById("quest").innerHTML = mydata[k]['question'];
+            let c = 0;
+            choice.forEach(option => {
+                c++;
+                option.innerHTML = mydata[k][`option${c}`]
+
+                j = 0;
+                progressBar.style.width = `${100 * (k + 1) / mydata.length}%`;
+                j++;
+            });
+            // downTimer();
+            optionclicked(mydata);
+            // console.log(mydata);
+        }
+
+    }
+    mydata.open('GET', 'assets/js/data.json', true);
+    mydata.send()
 }
 
+// function downTimer() {
+//     countDown.innerHTML = time;
+//     if (time < 10) {
+//         countDown.textContent = "0" + time;
+//     }
+//     time--;
+//     if (time >= 0) {
+//         setTimeout(downTimer, 1000);
+//     } else {
+//         corrections.push(mydata[k]['question'],mydata[k]['correction']);
+//         k++;
+//         if (k < lengthtable) {
+//             time = 30;
+//             GetQeustion();
+//             downTimer();
+//         }
+//         else {
+//             beforeResult();
+//         }
+
+//     }
+// }
 GetQeustion();
+function  optionclicked(mydata){
+    choice.forEach(option => {
+        option.onclick = () => {
+            // time=30;
+            // downTimer();
+            answered = option.textContent;
+            // console.log(answered);
+            correct  = mydata[k]['correct'];
+            // console.log(correct);
+    
+            if (answered == correct) {
+                count++;
+            }
+            else {
+                corrections.push([mydata[k]['question'],mydata[k]['correction']]);
+                // console.log(corrections);
+            }
+            k++;
+            console.log(k)
+            if (k < mydata.length) {
+                GetQeustion();
+            }
+            else {
+                beforeResult(mydata);
+            }
+        }
+    });
+}
 
-choice.forEach(option => {
-    option.onclick = () => {
-        time = 30;
-        answered = option.textContent.slice(0, 1);
-        const { correct } = questions[k].answer;
-        if (answered == correct) {
-            count++;
-        }
-        else {
-            corrections.push({ question: questions[k].question, answer: questions[k].answer.correction });
-        }
-
-        k++;
-        if (k < questions.length) {
-            GetQeustion();
-        }
-        else {
-            beforeResult();
-        }
-    }
-});
 $("#right").click(function () {
     circle[index].innerHTML = '<i class="fa-solid fa-check"></i>';
     circle[index].style.color = '#f8991c';
     $("#result").hide();
     $("#show-correction").show();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
